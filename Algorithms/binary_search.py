cb6bb7ba8c50cf994c86fed48943ce5a969383aa
joshuadavidang <joshuadavidang@outlook.sg>
2023-05-16 22:56:00 +0800

    feat: added test cases

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -3,18 +3,38 @@ class DSA:
         self.type = type
         self.name = name
 
-    def binary_search(self, data, target):
-        left, right = 0, len(data) - 1
+    def binary_search(self, books, target):
+        left, right = 0, len(books) - 1
         while left <= right:
             mid = (left + right) // 2
-            if data[mid] > target:
+            if books[mid] > target:
                 right = mid - 1          
-            elif data[mid] < target:
+            elif books[mid] < target:
                 left = mid + 1
             else:
                 return mid
+        return -1
     
-array = [1, 3, 5, 7, 11, 23, 35, 55, 56, 61, 33]
+tests = [
+    {
+        'name': 'query found in list book',
+        'input': {
+            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61, 33],
+            'query': 5
+        },
+        'output': 2
+        },
+    {
+        'name': 'query not found in list book',
+        'input': {
+            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61, 33],
+            'query': 50
+        },
+        'output': -1
+    }
+]
+                
 dsa = DSA("algorithm", "binary_search")
-index = dsa.binary_search(array, 5)
-print("Target found at index:", index)
+for i in range(len(tests)):
+    index = dsa.binary_search(tests[i]['input']['book_no'], tests[i]['input']['query'])
+    print(tests[i]['name'] + ":", tests[i]['output'] == index)
