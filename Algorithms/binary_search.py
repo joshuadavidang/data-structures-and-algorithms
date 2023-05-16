55c133e495b73e0c95186be4f3ea206920b7b297
joshuadavidang <joshuadavidang@outlook.sg>
2023-05-16 23:12:16 +0800

    test: update test cases

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -19,40 +19,64 @@ tests = [
     {
         'name': 'query found in list book',
         'input': {
-            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61, 33],
+            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61],
             'query': 5
         },
         'output': 2
         },
     {
+        'name': 'query found in the first element',
+        'input': {
+            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61],
+            'query': 1
+        },
+        'output': 0
+    },
+    {
+        'name': 'query found in the last element',
+        'input': {
+            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61],
+            'query': 61
+        },
+        'output': 9
+    },
+    {
         'name': 'query not found in list book',
         'input': {
-            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61, 33],
+            'book_no': [1, 3, 5, 7, 11, 23, 35, 55, 56, 61],
             'query': 50
         },
         'output': -1
     }
 ]
                 
-dsa = DSA("algorithm", "binary_search")
-for i in range(len(tests)):
-    result = dsa.binary_search(tests[i]['input']['book_no'], tests[i]['input']['query'])
-    print("Input:")
-    print(tests[i]['input'])
+dsa = DSA('algorithm', 'binary_search')
+passed_cases = 0
+failed_cases = 0
+total_tests = len(tests)
+
+for index, test in enumerate(tests):
+    result = dsa.binary_search(tests[index]['input']['book_no'], tests[index]['input']['query'])
+    print('Input:')
+    print(tests[index]['input'])
     print()
 
-    print("Expected Output:")
-    print(tests[i]['output'])
+    print('Expected Output:')
+    print(tests[index]['output'])
     print()
 
-    print("Actual Output:")
+    print('Actual Output:')
     print(result)
     print()
 
-    print("Test Result:")
-    if tests[i]['output'] == result:
-        print("PASSED")
+    print('Test Result:')
+    if tests[index]['output'] == result:
+        print('PASSED')
+        passed_cases += 1
     else:
-        print("FAILED")
+        print('FAILED')
+        failed_cases += 1
 
     print()
+
+print(passed_cases, '/', total_tests)
