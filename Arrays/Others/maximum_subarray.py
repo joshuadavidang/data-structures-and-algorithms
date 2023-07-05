47c100f7155b14a5661bac4565f38a7fd53581df
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-05 13:57:59 +0800

    feat: new lc problem

---
+++ Arrays/Others/maximum_subarray.py
@@ -0,0 +1,17 @@
+def maxSubArray(arr):
+    if len(arr) == 0:
+        return []
+
+    maxSum = arr[0]
+    currentSum = arr[0]
+
+    for i in range(1, len(arr)):
+        if currentSum < 0:
+            currentSum = arr[i]
+        else:
+            currentSum += arr[i]
+
+        if currentSum > maxSum:
+            maxSum = currentSum
+        
+    return maxSum
---
+++ Arrays/Others/maximum_subarray_test.py
@@ -0,0 +1,41 @@
+from maximum_subarray import maxSubArray
+
+tests = [
+    {
+        'test_case': 'Empty array []',
+        'query': [],
+        'output': []
+    },
+    {
+        'test_case': 'One element in array [5]',
+        'query': [5],
+        'output': 5
+    },
+    {
+        'test_case': 'Largest maximum sum of [-2,1,-3,4,-1,2,1,-5,4]',
+        'query': [-2,1,-3,4,-1,2,1,-5,4],
+        'output': 6
+    },
+]
+
+passed_cases = 0
+failed_cases = 0
+total_cases = len(tests)
+
+for i, test in enumerate(tests):
+    print(f'Test Case {i + 1}:', test['test_case'])
+    print(f'Expected Output:', test['output'])
+    result = maxSubArray(test['query'])
+    print(f'Actual Output:', result)
+    
+    if result == test['output']:
+        print("------ PASSED ------")
+        passed_cases += 1
+        print()
+    else:
+        print("------ FAILED ------")
+        failed_cases += 1
+        print()
+
+print('*' * 100, '\n')
+print(f'Result: {passed_cases}/{total_cases} \n')
