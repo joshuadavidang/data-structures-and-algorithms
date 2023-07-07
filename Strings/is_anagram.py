5eeea023b191430e82fc36f65870b5786941e761
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-07 10:49:19 +0800

    feat: added new lc problem

---
+++ Strings/is_anagram.py
@@ -0,0 +1,25 @@
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    
+    freq = {}
+
+    for char in s:
+        if char not in freq:
+            freq[char] = 1
+        else:
+            freq[char] += 1
+
+    for char in t:
+        if char in freq:
+            freq[char] -= 1
+            if freq[char] < 0:
+                return False
+        else:
+            return False
+        
+    for value in freq.values():
+        if value != 0:
+            return False
+        
+    return True
