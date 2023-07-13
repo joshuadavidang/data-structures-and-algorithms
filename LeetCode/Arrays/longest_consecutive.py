392630f4e38873f9d2b22c0c83a36adf258df807
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-13 16:17:33 +0800

    feat: new lc problem

---
+++ LeetCode/Arrays/longest_consecutive.py
@@ -0,0 +1,12 @@
+def longestConsecutive(nums):
+    numSet = set(nums)
+    longest = 0
+
+    for num in nums:
+        if (num - 1) not in numSet:
+            length = 0
+            while (num + length) in numSet:
+                length += 1
+            if length > longest:
+                longest = length
+    return longest
---
+++ LeetCode/Arrays/longest_consecutive_test.py
@@ -0,0 +1,26 @@
+from longest_consecutive import longestConsecutive
+
+tests = [
+    {"query": [100, 4, 200, 1, 3, 2], "output": 4},
+    {"query": [0, 3, 7, 2, 5, 8, 4, 6, 0, 1], "output": 9},
+]
+
+passed_cases = 0
+failed_cases = 0
+total_cases = len(tests)
+
+for i, test in enumerate(tests):
+    print(f"Test Case {i + 1}")
+    print(f"Expected Output:", test["output"])
+    result = longestConsecutive(test["query"])
+    print(f"Actual Output:", result)
+
+    if result == test["output"]:
+        print("------ PASSED ------ \n")
+        passed_cases += 1
+    else:
+        print("------ FAILED ------ \n")
+        failed_cases += 1
+
+print("*" * 100, "\n")
+print(f"Result: {passed_cases}/{total_cases} \n")
