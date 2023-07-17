2ddfd788bf1010ef1be03129fab9ccc69402ad65
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-17 16:09:12 +0800

    fix: update solution

--- LeetCode/Binary_Search/find_min.py
+++ LeetCode/Binary_Search/find_min.py
@@ -1,22 +1,18 @@
-# def find_min(nums):
-#     result = nums[0]
-#     left, right = 0, len(nums) - 1
+def find_min(nums):
+    result = nums[0]
+    left, right = 0, len(nums) - 1
 
-#     while left <= right:
-#         if nums[left] <= nums[right]:
-#             result = min(result, nums[left])
-#             break
+    while left <= right:
+        if nums[left] <= nums[right]:
+            result = min(result, nums[left])
+            break
 
-#         mid = (left + right) // 2
-#         result = min(result, nums[mid])
+        mid = (left + right) // 2
+        result = min(result, nums[mid])
 
-#         if nums[mid] > nums[left]:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
+        if nums[mid] > nums[left]:
+            left = mid + 1
+        else:
+            right = mid - 1
 
-#     return result
-
-arr = [1, 2]
-if 0 not in arr:
-    print("yea")
+    return result
