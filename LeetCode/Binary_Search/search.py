b715f60a64d373c9bad5c5dce2e4e5087b87b32b
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-17 16:07:46 +0800

    fix: update solution

--- LeetCode/Binary_Search/search.py
+++ LeetCode/Binary_Search/search.py
@@ -9,14 +9,17 @@ def search(nums, target):
         # left sorted position
         if nums[left] <= nums[mid]:
             if nums[mid] < target or nums[left] > target:
+                # ignore left, move towards right
                 left = mid + 1
             else:
+                # ignore right, move towards left
                 right = mid - 1
 
+        # right sorted position
         else:
-            if nums[mid] < target or nums[right] < target:
-                left = mid + 1
+            if nums[mid] > target or nums[right] < target:
+                right = mid - 1
             else:
-                right = mid + 1
+                left = mid + 1
 
     return -1
