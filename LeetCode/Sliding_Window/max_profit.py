563103c9118c6b13852db3187536c2b8e799d798
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-18 15:51:50 +0800

    feat: added new lc problem

---
+++ LeetCode/Sliding_Window/max_profit.py
@@ -0,0 +1,15 @@
+def max_profit(prices):
+    # left: buy, right: sell
+    left, right = 0, 1
+    maxProfit = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            maxProfit = max(maxProfit, profit)
+        else:
+            left = right
+
+        right += 1
+
+    return maxProfit
