ed30a1664cdbfd400d44fa23f0be9b5813f3c140
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-18 15:16:39 +0800

    docs: added comments

--- LeetCode/Arrays/longest_consecutive.py
+++ LeetCode/Arrays/longest_consecutive.py
@@ -1,9 +1,11 @@
 def longest_consecutive(nums):
+    # remove any duplicates
     numSet = set(nums)
     longest = 0
 
     for num in nums:
         if (num - 1) not in numSet:
+            # current num is the start of a sequence
             length = 0
             while (num + length) in numSet:
                 length += 1
