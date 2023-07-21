7c61cb8649d555a7ed123f5f3c86b785f3cf9f4c
joshuadavidang <joshuadavidang@outlook.sg>
2023-07-21 17:19:55 +0800

    fix: update group anagram solutions

--- LeetCode/Arrays/group_anagram.py
+++ LeetCode/Arrays/group_anagram.py
@@ -1,14 +1,16 @@
-from collections import defaultdict
-
-
 def group_anagram(strs):
-    dict_data = defaultdict(list)
+    dict_data = {}
+
     for word in strs:
-        key = "".join(sorted(word))
+        sorted_word = sorted(word)
+        key = "".join(sorted_word)
+        if key not in dict_data:
+            dict_data[key] = []
+
         dict_data[key].append(word)
 
     result = []
-    for value in dict_data.values():
-        result.append(value)
+    for values in dict_data.values():
+        result.append(values)
 
     return result
