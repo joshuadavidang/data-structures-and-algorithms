dc9c1c20953ae69664d502c68cc9394f61dfcdcc
joshuadavidang <joshuadavidang@outlook.sg>
2023-08-01 14:31:44 +0800

    feat: updated solution for find_anagrams

--- LeetCode/Two_Pointer/find_anagrams.py
+++ LeetCode/Two_Pointer/find_anagrams.py
@@ -23,12 +23,12 @@ def findAnagrams(s, p):
     k = len(p)
 
     for right in range(k, len(s)):
+        s_count[s[left]] -= 1
+
         if s[right] not in s_count:
             s_count[s[right]] = 1
         else:
             s_count[s[right]] += 1
-
-        s_count[s[left]] -= 1
 
         if s_count[s[left]] == 0:
             s_count.pop(s[left])
